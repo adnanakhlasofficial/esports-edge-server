ff40ec4ace9f9ef2721757157e708c7b1a0ca113
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-12-04 11:44:31 +0600

    Implemented equipment post request

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0bmcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb://localhost:27017/";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +26,18 @@ async function run() {
         await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
+
+        const equipmentCollection = client
+            .db("equipmentDB")
+            .collection("equipment");
+
+        app.post("/equipments", async (req, res) => {
+            const equipment = req.body;
+            const result = await equipmentCollection.insertOne(equipment);
+            console.log(result);
+            res.send(result);
+        });
+
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
