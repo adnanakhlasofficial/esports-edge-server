f16bbafcd2c1caf320c3a84bce59b3708d96f53c
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-12-05 22:57:39 +0600

    URI updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const uri = "mongodb://localhost:27017/";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0bmcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -43,7 +43,7 @@ async function run() {
             res.send(result);
         });
 
-        app.put("/equipment/:id", async (req, res) => {
+        app.put("/updateEquipment/:id", async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const options = { upsert: true };
