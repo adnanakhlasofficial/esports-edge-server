29eaafbabc22bebdcbcffd98e5adb4841bfc4d8a
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-12-06 14:49:08 +0600

    Implemented delete method

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0bmcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb://localhost:27017/";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -75,6 +75,13 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await equipmentCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.delete("/equipment/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await equipmentCollection.deleteOne(query);
             res.send(result);
         });
 
